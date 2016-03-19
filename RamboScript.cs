07db38662b0d109180c44c30c0a1d88de848250d
mforte2929 <mforte2929@users.noreply.github.com>
2016-03-19 15:06:14 +0000

    Added files via upload
    Scripts from V1.25

---
+++ RamboScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RamboScript : MonoBehaviour 
+{
+	public Slider ramboSlider;
+	public Text ramboText;
+	public GameObject ramzooka;
+
+	SpriteRenderer headbandSprite;
+	CircleCollider2D headbandCollider;
+	GameObject ramboHeadband;
+	SheepController sheepController;
+	SheepStamina sheepStamina;
+	bool headBandActive;
+	
+	void Awake () 
+	{
+		ramboHeadband = GameObject.FindGameObjectWithTag ("RamboHeadband");
+		ramzooka = GameObject.FindGameObjectWithTag ("Ramzooka");
+		sheepController = FindObjectOfType<SheepController> ();
+		sheepStamina = FindObjectOfType<SheepStamina> ();
+		headbandSprite = ramboHeadband.GetComponent<SpriteRenderer>();
+		headbandCollider = ramboHeadband.GetComponent<CircleCollider2D>();
+	}
+
+	void RamboBazooka()
+	{
+		if(headBandActive == true)
+		{
+			ramzooka.SetActive(true);
+		}
+		else
+		{
+			ramzooka.SetActive(false);
+		}
+	}
+	
+	void FixedUpdate () 
+	{
+		if(headBandActive == true)
+		{
+			sheepController.jumpPower = 700f;
+			ramboSlider.value -= Time.deltaTime;
+			ramboText.text = ("RAMBO ACTIVE! " + ramboSlider.value.ToString("f0"));
+		}
+
+		if(ramboSlider.value == 0)
+		{
+			headBandActive = false;
+			sheepController.jumpPower = 400f;
+			sheepController.maxSpeed = 5;
+		}
+
+		RamboBazooka ();
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Sheep") 
+		{
+			headBandActive = true;
+			headbandSprite.enabled = false;
+			headbandCollider.enabled = false;
+
+		}
+		
+	}
+
+}
---
+++ ShootBullet.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBullet : MonoBehaviour 
+{
+	/*
+
+	GameObject woolBullet;
+	float attackSpeed = 2f;
+	float coolDown;
+
+	void Awake () 
+	{
+		woolBullet = GameObject.FindGameObjectWithTag("RamzookaBullet");
+	
+	}
+	
+
+	void Update () 
+	{
+		if(Time.time >= coolDown) 
+		{
+			if(Input.GetKeyDown (KeyCode.F)) 
+			{
+				Fire();
+				Debug.Log ("Shoot");
+			}
+		}
+	}
+
+
+	void Fire() 
+	{
+		GameObject woolBullet = Instantiate(woolBullet, transform.position, Quaternion.identity) as GameObject;
+
+		woolBullet.AddForce (Vector3.up * 100f);
+			
+		cooldown = Time.time + attackSpeed;
+	}
+
+
+*/
+
+}
